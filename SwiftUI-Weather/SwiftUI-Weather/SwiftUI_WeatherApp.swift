d78b514c20a64b7a39d265962ef103fc4c109c4b
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-24 20:34:33 +0200

    Setting up

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Erik Sebastian de Erice Jerez on 24/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
