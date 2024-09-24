77d29936ff0cf2e965bda97c008e8acc1e77f33c
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-24 21:30:42 +0200

    Creating an HStack for days of the week

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(
-                colors: [.blue, .white],
+                colors: [.blue, Color("lightBlue")],
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing)
             .ignoresSafeArea()
@@ -22,7 +22,7 @@ struct ContentView: View {
                     .foregroundStyle(.white)
                     .padding()
                 
-                VStack(spacing: 10) {
+                VStack(spacing: 8) {
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
                         .resizable()
@@ -30,8 +30,36 @@ struct ContentView: View {
                         .frame(width: 180, height: 180)
                     
                     Text("76°")
-                        .font(.system(size: 70, weight: .medium))
+                        .font(.system(size: 64, weight: .medium))
                         .foregroundStyle(.white)
+                }
+                .padding(.bottom, 48)
+                
+                HStack(spacing: 24) {
+                    WeatherDayView(
+                        dayOfWeek: "TUE",
+                        imageName: "cloud.sun.fill",
+                        temperature: 74)
+                    
+                    WeatherDayView(
+                        dayOfWeek: "WED",
+                        imageName: "sun.max.fill",
+                        temperature: 88)
+                    
+                    WeatherDayView(
+                        dayOfWeek: "THU",
+                        imageName: "wind.snow",
+                        temperature: 55)
+                    
+                    WeatherDayView(
+                        dayOfWeek: "FRI",
+                        imageName: "sunset.fill",
+                        temperature: 60)
+                    
+                    WeatherDayView(
+                        dayOfWeek: "SAT",
+                        imageName: "snow",
+                        temperature: 25)
                 }
                 
                 Spacer()
@@ -42,4 +70,28 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium))
+                .foregroundStyle(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)")
+                .font(.system(size: 24, weight: .medium))
+                .foregroundStyle(.white)
+        }
+    }
 }
