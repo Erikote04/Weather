cb813fcb4bcc8ac0acd25dbec5988895ceea767a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-24 21:10:36 +0200

    Creating an image with SF Symbols

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -18,9 +18,21 @@ struct ContentView: View {
             
             VStack {
                 Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .font(.system(size: 32, weight: .medium))
                     .foregroundStyle(.white)
                     .padding()
+                
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundStyle(.white)
+                }
                 
                 Spacer()
             }
